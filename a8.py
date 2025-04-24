5f881142a135a56b6bcb44f59801a558c6257ae3
Nathan <nchen6@cps.edu>
2025-04-24 10:13:36 -0500

    updated

--- a8.py
+++ a8.py
@@ -9,3 +9,10 @@ xorn.train(xor_training_data)
 print(xorn.test_with_expected(xor_training_data))
 
 
+party_training_data = [([0.9, 0.6,0.8,0.3,0.1], [1.0]), ([0.8 , 0.8, 0.4, 0.6, 0.4], [1.0]), ([0.7, 0.2, 0.4, 0.6, 0.3], [1.0]), ([0.5, 0.5, 0.8, 0.4, 0.8], [0.0]), ([0.3, 0.1, 0.6, 0.8, 0.8], [0.0]), ([0.6, 0.3, 0.4, 0.3, 0.6], [0.0])]
+
+party_testing_data = [([1.0, 1.0, 1.0, 0.1, 0.1], []), ([0.5, 0.2, 0.1, 0.7, 0.7], []), ([0.8,0.3,0.3,0.3,0.8], []), ([0.8,0.3,0.3,0.8,0.3], []), ([0.9,0.8,0.8,0.3,0.6], [])]
+
+polNet = NeuralNet(5,2,1)
+polNet.train(party_training_data, iters= 1000, print_interval = 100)
+print(polNet.test_with_expected(party_testing_data))
